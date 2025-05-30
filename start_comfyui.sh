3562fe459d18abbafbd5ca2ac60f6f5e2e99dab9
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-05-31 02:35:07 +0900

    [modify] Merged start_comfyui_fp16.sh & start_comfyui_fp32.sh

--- start_comfyui.sh
+++ start_comfyui.sh
@@ -2,6 +2,18 @@
 
 cd /opt/ComfyUI-running-on-Podman-WSL2
 
+ARGS="--force-fp16"
+# --force-fp32 が引数に含まれている場合、ARGSに"--force-fp32"をセット
+for arg in "$@"; do
+  if [ "$arg" = "--force-fp32" ]; then
+    ARGS="--force-fp32"
+    break
+  fi
+done
+
+# dont-print-server オプションを追加
+ARGS="${ARGS} --dont-print-server"
+
 # ComfyUIのコンテナを実行
 # WSL2起動時に実行すればOK
 podman run -d --rm \
@@ -10,5 +22,5 @@ podman run -d --rm \
   --volume "/opt/ComfyUI-running-on-Podman-WSL2/data:/data" \
   --volume "/opt/ComfyUI-running-on-Podman-WSL2/output:/output" \
   --device "nvidia.com/gpu=all" \
-  -e CLI_ARGS="--force-fp32 --dont-print-server" \
+  -e CLI_ARGS="${ARGS}" \
   localhost/comfyui:v0.3.39
