c0492fa59d6f700a87c52a7c4dcc2466ae6f9b54
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-08-05 06:27:07 +0900

    [modify] Update build.sh

--- build.sh
+++ build.sh
@@ -10,22 +10,22 @@ fi
 # モデルをダウンロードするためのコンテナをビルド
 podman build -t model-downloader:latest \
   --force-rm \
-  --volume "/opt/ComfyUI-running-on-Podman-WSL2/data:/data" \
+  --volume "$(pwd)/data:/data" \
   ./services/download/
 
 # モデルをダウンロードするためのコンテナを実行
 # 初回にだけ実行
 podman run -it --rm \
   --name model-downloader \
-  --volume "/opt/ComfyUI-running-on-Podman-WSL2/data:/data" \
+  --volume "$(pwd)/data:/data" \
   localhost/model-downloader:latest
 
 # ComfyUIのコンテナをビルド
 podman build -t comfyui:${COMFYUI_TAG} \
   --force-rm \
   --build-arg COMFYUI_TAG=${COMFYUI_TAG} \
-  --volume "/opt/ComfyUI-running-on-Podman-WSL2/data:/data" \
-  --volume "/opt/ComfyUI-running-on-Podman-WSL2/output:/output" \
+  --volume "$(pwd)/data:/data" \
+  --volume "$(pwd)/output:/output" \
   --device "nvidia.com/gpu=all" \
   ./services/comfyui/
 
@@ -34,5 +34,5 @@ if [ ! -d "./output/config/custom_nodes/ComfyUI-Manager" ]; then
   git clone https://github.com/ltdrdata/ComfyUI-Manager.git ./output/config/custom_nodes/ComfyUI-Manager
   cd ./output/config/custom_nodes/ComfyUI-Manager
   git fetch --tags
-  git checkout tags/3.32.5
+  git checkout tags/3.35
 fi
